a22248020e992084c6c1b60e88bbc7301d6571de
Thiago Cardoso <thiago.moura.cardoso@gmail.com>
2025-04-23 21:12:18 -0300

    Observando como funciona o operador de incremento e decremento

---
+++ IncrementoEDecremento.java
@@ -0,0 +1,12 @@
+public class IncrementoEDecremento {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println("Valor inicial da variável x = " + x);
+
+        x++;
+        System.out.println("Valor com incremento(++) = " + x );
+
+        x--;
+        System.out.println("Valor com decremento(--) = " + x);
+    }
+}
