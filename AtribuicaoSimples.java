2e1ffe65f487cf4a7b4eb2417679b591ee554692
Thiago Cardoso <thiago.moura.cardoso@gmail.com>
2025-04-23 21:01:36 -0300

    Fazendo a atribuição simples em uma variável

---
+++ AtribuicaoSimples.java
@@ -0,0 +1,21 @@
+public class AtribuicaoSimples {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println("O valor inicial de x = " + x);
+
+        x += 5;
+        System.out.println("O valor de x + 5 = " + x);
+
+        x -= 2;
+        System.out.println("O valor de x - 2 = " + x);
+
+        x *= 3;
+        System.out.println("O valor de x * 3 = " + x);
+
+        x /= 3;
+        System.out.println("O valor de x / 3 = " + x);
+
+        x %= 5;
+        System.out.println("O valor de x % 5 = " + x );
+    }
+}
